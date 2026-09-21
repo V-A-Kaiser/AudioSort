513a3b3f95583aa2b4b76a71b6b36ae56b481589
Valerie Kaiser <github@vakaiser.com>
2026-09-21 00:56:28 -0700

    fix(controls): reword the Beat Slice tooltip

--- src/lib/components/Controls.svelte
+++ src/lib/components/Controls.svelte
@@ -206,7 +206,7 @@
           >Beat Slice</label
         >
         {@render info(
-          "Starts the slicing just before detected drum transients. Useful for slicing tracks with consistent rhythmic elements. Use the offset value to shift where the start occurs.",
+          "Start chunk slicing at the first detected drum transient. Useful for slicing tracks with consistent rhythmic elements. Use the offset value to shift where the start occurs.",
           "start"
         )}
       </span>
